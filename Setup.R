d6f33a6dd61a28013211fe7ded8cef3bb2208637
Kevin Errico <Kevin Errico>
2016-08-03 21:27:21 +0100

    first commit

---
+++ Setup.R
@@ -0,0 +1,6 @@
+library(twitteR)
+consumer_key = " 0dNKgfWBE8bKAcBfL8xyzhiog"
+consumer_secret = "1BrIkmUDhTAA6LR0FjKz00lr9gl8fSEbHdqCTbz2lD0A9BzEWn"
+access_token = "760930056487182336-dZIccBHYOKQiSNEBS48nVQYiEH5sWgR"
+access_secret = "TqxhqyjUdKmEUQ8lZcuhdVSnpBLFlsmBfFtJEasGOPmXx"
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
