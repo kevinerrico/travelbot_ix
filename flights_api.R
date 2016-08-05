9042616b09847a39f600bd943092643e8f9e922c
Rails Udemy Course <sebastian.alvarado@mail.utoronto.ca>
2016-08-05 17:46:28 +0200

    Working API request and having the price needed

--- flights_api.R
+++ flights_api.R
@@ -1,15 +1,42 @@
 #Google Flights API interactions
 library(httr)
 library(dplyr)
-KEY = AIzaSyAE-ngDIn5Dau4aNCzf2FqbVeIH9iHQB9o
+library(jsonlite)
+library(lubridate)
+KEY = 'AIzaSyAE-ngDIn5Dau4aNCzf2FqbVeIH9iHQB9o'
+DEFAULT_ORIGIN = "CPT"
 
 
-choose_origin <- function(x){
-  
+choose_destination <- function(){
+  #Dataset loading
+  dest_data <- read_csv("Airports.csv")
+  random_row <- sample_n(dest_data,1,replace = TRUE)
+  #Get the airport code
+  airport <- random_row$CODE
+  return(airport)
 }
-prepare_body <- function(x){
-  
+prepare_body <- function(){
+  #Get JSON object
+  body <- fromJSON("request.json")
+  todays_date <- today(tzone = "CET")
+  flight_date <- todays_date + days(7)
+  body$request$slice$origin <- DEFAULT_ORIGIN
+  body$request$slice$destination <- choose_destination()
+  body$request$slice$date <- flight_date
+  body$request$solutions <- 1
+  return(body)
 }
 produce_request <- function(){
-  
+  body <- prepare_body()
+  url <- sprintf("https://www.googleapis.com/qpxExpress/v1/trips/search?key=%s",KEY)
+  jsonBody <- toJSON(body)
+  request <- POST(url,body=body, encode = "json",verbose())
+  status <- http_status(request)
+  print(request)
+  if(status$category == "Success"){
+    data <- content(request, as = "text", encoding = "UTF-8")
+    flights_data <- fromJSON(data)
+    price <- flights_data$trips$tripOption$saleTotal
+    return(price)
+  }
 }
